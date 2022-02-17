e4aa723606a71a9ef73a80e226cdcbadc6c41fb4
Ibonane <bonanedjimbala@gmail.com>
2022-02-17 17:29:53 +0100

    seconde commit

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -11,7 +11,10 @@
     <p>Désolé Aucun Article trouvé pour cette Recherche...</p>
     @endif  
     @foreach ($queries as $query)
-        <h3>{{ $query->id }}- {{ $query->name }} :  {{ $query->price }} $</h3>
+        <h3>
+            {{ $query->id }}- {{ $query->name }} :  {{ $query->price }} $
+            <a href = '/article/{{$query->id}}' style="color:orange">Detail</a>
+        </h3>
     @endforeach
 </div>
 @endsection
